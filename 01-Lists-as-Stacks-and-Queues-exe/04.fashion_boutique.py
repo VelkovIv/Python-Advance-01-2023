7f0f97c8cebc3d4be14fd26f935f25dc5129ce98
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-12 22:41:03 +0200

    Update 04.fashion_boutique.py

--- 01-Lists-as-Stacks-and-Queues-exe/04.fashion_boutique.py
+++ 01-Lists-as-Stacks-and-Queues-exe/04.fashion_boutique.py
@@ -6,14 +6,12 @@ current_rack = 0
 while box_with_clothes:
     current_clothing = int(box_with_clothes.pop())
 
+    if rack_number == 0:
+        rack_number = 1
+
     if rack_size - current_rack >= current_clothing:
-        if current_rack == 0:
-            rack_number += 1
         current_rack += current_clothing
 
-        if rack_size == current_rack:
-            current_rack = 0
-            rack_number += 1
     else:
         current_rack = 0
         rack_number += 1
