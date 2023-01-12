301cd0bd7fd4c0f364c37305eee009739cb9bb00
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-12 22:40:37 +0200

    Update 02.stacked-queries.py

--- 01-Lists-as-Stacks-and-Queues-exe/02.stacked-queries.py
+++ 01-Lists-as-Stacks-and-Queues-exe/02.stacked-queries.py
@@ -8,9 +8,9 @@ for num in range(number_of_queries):
         stack.append(int(current_query[1]))
     elif current_query[0] == "2" and len(stack) > 0:
         stack.pop()
-    elif current_query[0] == "3":
+    elif current_query[0] == "3" and len(stack) > 0:
         print(max(stack))
-    elif current_query[0] == "4":
+    elif current_query[0] == "4" and len(stack) > 0:
         print(min(stack))
 
 if len(stack) > 0:
