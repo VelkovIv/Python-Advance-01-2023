3d21970a9f082aa49876676755a7a3e74f4fe458
VelkovIv <ivaylo.velkov@gmail.com>
2023-02-07 23:50:10 +0200

    Create modules.py

---
+++ 09-Modules_lab/modules.py
@@ -0,0 +1,13 @@
+def print_triangle(num):
+    result = ''
+    for n in range(1, num + 1):
+        for m in range(1, n + 1):
+            result += f'{m} '
+        result += '\n'
+
+    for n in range(num - 1, 0, -1):
+        for m in range(1, n + 1):
+            result += f'{m} '
+        result += '\n'
+
+    return result
