704fb59bf6ee1a4cf3a86276ae2e1fe21902a547
VelkovIv <ivaylo.velkov@gmail.com>
2023-02-13 20:05:37 +0200

    Update 01.flowers_finder.py

--- 10-Exam-Preparation/01.flowers_finder.py
+++ 10-Exam-Preparation/01.flowers_finder.py
@@ -1,9 +1,9 @@
 from collections import deque
 
 vowels = deque(list(input().split()))
-consonants = deque(list(input().split()))
+consonants = list(input().split())
 flower_found = False
-flowers_to_check = {'rose': [], 'tulip': [], 'lotus': [], 'daffodil': []}
+flowers_to_check = {'rose': 'rose', 'tulip': 'tulip', 'lotus': 'lotus', 'daffodil': 'daffodil'}
 f_counter = 0
 
 while vowels and consonants:
@@ -11,18 +11,14 @@ while vowels and consonants:
     current_consonant = consonants.pop()
 
     for flower, name in flowers_to_check.items():
-        if current_vowel in flower and current_vowel not in name:
-            flowers_to_check[flower].append(current_vowel)
+        if current_vowel in flower:
+            flowers_to_check[flower] = flowers_to_check[flower].replace(current_vowel, '')
 
         if current_consonant in flower:
-            if current_consonant == 'f' and f_counter <= 2:
-                flowers_to_check[flower].append(current_consonant)
-                f_counter += 1
-            elif current_consonant not in name:
-                flowers_to_check[flower].append(current_consonant)
+            flowers_to_check[flower] = flowers_to_check[flower].replace(current_consonant, '')
 
     for flower, name in flowers_to_check.items():
-        if len(flower) == len(name):
+        if len(name) == 0:
             print(f"Word found: {flower}")
             flower_found = True
             break
