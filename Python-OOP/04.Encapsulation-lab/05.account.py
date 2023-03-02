ae62a6c3c55fe387f6187297944999ecf4c51864
VelkovIv <ivaylo.velkov@gmail.com>
2023-03-02 22:32:45 +0200

    Create 05.account.py

---
+++ Python-OOP/04.Encapsulation-lab/05.account.py
@@ -0,0 +1,28 @@
+class Account:
+    def __init__(self, user_id: int, balance: float, pin: int):
+        self.__id = user_id
+        self.__pin = pin
+        self.balance = balance
+
+    def get_id(self, pin: int) -> [str, int]:
+        if pin == self.__pin:
+
+            return self.__id
+
+        return "Wrong pin"
+
+    def change_pin(self, old_pin: int, new_pin: int) -> str:
+        if old_pin == self.__pin:
+            self.__pin = new_pin
+
+            return "Pin changed"
+
+        return "Wrong pin"
+
+
+account = Account(8827312, 100, 3421)
+print(account.get_id(1111))
+print(account.get_id(3421))
+print(account.balance)
+print(account.change_pin(2212, 4321))
+print(account.change_pin(3421, 1234))
