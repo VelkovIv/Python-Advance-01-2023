a0b538df01426b815fc4f64abe04182dc76c6de6
VelkovIv <ivaylo.velkov@gmail.com>
2023-03-05 23:51:43 +0200

    Update 02.shop.py

--- Python-OOP/05.Static-and-Class-Methods-lab/02.shop.py
+++ Python-OOP/05.Static-and-Class-Methods-lab/02.shop.py
@@ -1,38 +1,39 @@
 class Shop:
     def __init__(self, name: str, shop_type: str, capacity: int):
         self.name = name
-        self.shop_type = shop_type
+        self.type = shop_type
         self.capacity = capacity
         self.items = {}  # {item_name: item_quantity}
+        self.current_capacity = capacity
 
     @classmethod
     def small_shop(cls, name: str, shop_type: str):
         return cls(name, shop_type, 10)
 
     def add_item(self, item_name: str) -> str:
-        if self.capacity > sum(self.items.values()):
-            if item_name not in self.items:
-                self.items[item_name] = 0
-            self.items[item_name] += 1
+        if self.current_capacity == 0:
+            return "Not enough capacity in the shop"
 
-            return f"{item_name} added to the shop"
+        if item_name not in self.items:
+            self.items[item_name] = 0
+        self.items[item_name] += 1
 
-        return "Not enough capacity in the shop"
+        self.current_capacity -= 1
+
+        return f"{item_name} added to the shop"
 
     def remove_item(self, item_name: str, amount: int) -> str:
-        if item_name in self.items:
-            if self.items[item_name] >= amount:
-                self.items[item_name] -= amount
-                if self.items[item_name] == 0:
-                    del self.items[item_name]
+        if item_name not in self.items or amount > self.items[item_name]:
+            return f"Cannot remove {amount} {item_name}"
 
-                return f"{amount} {item_name} removed from the shop"
+        self.items[item_name] -= amount
+        if self.items[item_name] == 0:
+            self.items.pop(item_name)
 
-        return f"Cannot remove {amount} {item_name}"
+        return f"{amount} {item_name} removed from the shop"
 
     def __repr__(self):
-        return f"{self.name} of type {self.shop_type} with capacity {self.capacity}"
-
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
 
 # fresh_shop = Shop("Fresh Shop", "Fruit and Veg", 50)
 # small_shop = Shop.small_shop("Fashion Boutique", "Clothes")
