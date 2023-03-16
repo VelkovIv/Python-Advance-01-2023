b52a7d734ff7a2d433267582b292a1d92851268e
VelkovIv <ivaylo.velkov@gmail.com>
2023-03-17 00:12:49 +0200

    Create workers.py

---
+++ Python-OOP/07.SOLID-exe/workers.py
@@ -0,0 +1,60 @@
+from abc import abstractmethod, ABC
+
+
+class MainWorker(ABC):
+
+    @abstractmethod
+    def work(self):
+        pass
+
+
+class Worker(MainWorker):
+
+    def work(self):
+        print("I'm working!!")
+
+
+class SuperWorker(MainWorker):
+
+    def work(self):
+        print("I work very hard!!!")
+
+
+class Manager:
+
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        assert isinstance(worker, MainWorker), f'`worker` must be of type {MainWorker}'
+
+        self.worker = worker
+
+    def manage(self):
+        if self.worker is not None:
+            self.worker.work()
+
+
+# worker = Worker()
+# manager = Manager()
+# manager.set_worker(worker)
+# manager.manage()
+#
+# super_worker = SuperWorker()
+# try:
+#     manager.set_worker(super_worker)
+# except AssertionError:
+#     print("manager fails to support super_worker....")
+
+
+worker = Worker()
+manager = Manager()
+manager.set_worker(worker)
+manager.manage()
+
+super_worker = SuperWorker()
+try:
+    manager.set_worker(super_worker)
+    manager.manage()
+except AssertionError:
+    print("manager fails to support super_worker....")
