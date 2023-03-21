fe0943ebea64879ed7b1c19621f3c646a63ddf6f
VelkovIv <ivaylo.velkov@gmail.com>
2023-03-21 21:22:10 +0200

    Create 04.multiply.py

---
+++ Python-OOP/09.Decorators_lab/04.multiply.py
@@ -0,0 +1,26 @@
+def multiply(times):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            result = function(*args, **kwargs)
+
+            return result * times
+
+        return wrapper
+
+    return decorator
+
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(3))
+
+
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(6))
