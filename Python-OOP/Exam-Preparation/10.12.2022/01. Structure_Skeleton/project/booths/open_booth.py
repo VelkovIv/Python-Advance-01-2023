3b38c048c7efb569318f096685eac062324c75d7
VelkovIv <ivaylo.velkov@gmail.com>
2023-04-01 21:29:55 +0300

    Create open_booth.py

---
+++ Python-OOP/Exam-Preparation/10.12.2022/01. Structure_Skeleton/project/booths/open_booth.py
@@ -0,0 +1,10 @@
+from project.booths.booth import Booth
+
+
+class OpenBooth(Booth):
+    PRICE_PER_PERSON: float = 2.5
+
+    def reserve(self, number_of_people: int):
+        self.price_for_reservation = number_of_people * self.PRICE_PER_PERSON
+        self.is_reserved = True
+
