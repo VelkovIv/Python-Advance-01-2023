885406ac7e2aae86bde5248235ca955cd2d27b29
VelkovIv <ivaylo.velkov@gmail.com>
2023-04-04 22:27:15 +0300

    Create movie.py

---
+++ Python-OOP/Exam-Preparation/18.04.2022/01. Structure_Skeleton/project/movie_specification/movie.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+
+
+class Movie(ABC):
+
+    def __init__(self, title: str, year: int, owner: object, age_restriction: int):
+        self.title = title
+        self.year = year
+        self.owner = owner  # User
+        self.age_restriction = age_restriction
+        self.likes: int = 0
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, value: str):
+        if value.strip() == '':
+            raise ValueError("The title cannot be empty string!")
+        self.__title = value
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, value: int):
+        if value < 1888:
+            raise ValueError("Movies weren't made before 1888!")
+        self.__year = value
+
+    @property
+    def owner(self):
+        return self.__owner
+
+    @owner.setter
+    def owner(self, value: object):
+        if not type(value).__name__ == 'User':
+            raise ValueError("The owner must be an object of type User!")
+
+        self.__owner = value
+
+    @abstractmethod
+    def details(self):
+        ...
