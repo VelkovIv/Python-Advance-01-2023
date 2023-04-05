955bce07ea7d54085e530ce7201df874cb69d5a9
VelkovIv <ivaylo.velkov@gmail.com>
2023-04-05 23:50:29 +0300

    Create race.py

---
+++ Python-OOP/Exam-Preparation/11.12.2021/01. Structure_Skeleton/project/race.py
@@ -0,0 +1,19 @@
+from typing import List
+from project.driver import Driver
+
+
+class Race:
+    def __init__(self, name: str):
+        self.name = name
+        self.drivers: List[Driver] = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if value.strip() == "":
+            raise ValueError("Name cannot be an empty string!")
+
+        self.__name = value
