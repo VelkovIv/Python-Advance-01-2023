ac5c8b724812452b16cf135e8640e67326299386
VelkovIv <ivaylo.velkov@gmail.com>
2023-04-05 20:56:24 +0300

    Create movie.py

---
+++ Python-OOP/Exam-Preparation/10.04.2022/03. Unit Testing_Skeleton/project/movie.py
@@ -0,0 +1,52 @@
+class Movie:
+    def __init__(self, name: str, year: int, rating: float):
+        self.name = name
+        self.year = year
+        self.rating = rating
+        self.actors = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == '':
+            raise ValueError("Name cannot be an empty string!")
+        self.__name = value
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, value):
+        if value < 1887:
+            raise ValueError("Year is not valid!")
+        self.__year = value
+
+    def add_actor(self, name: str):
+        if name not in self.actors:
+            self.actors.append(name)
+        else:
+            return f"{name} is already added in the list of actors!"
+
+    def __gt__(self, other):
+        if self.rating > other.rating:
+            return f'"{self.name}" is better than "{other.name}"'
+        else:
+            return f'"{other.name}" is better than "{self.name}"'
+
+    def __repr__(self):
+        return f"Name: {self.name}\n" \
+               f"Year of Release: {self.year}\n" \
+               f"Rating: {self.rating:.2f}\n" \
+               f"Cast: {', '.join(self.actors)}"
+
+
+m = Movie("Just Movie", 2020, 6.7)
+m.add_actor('Actor1')
+m.add_actor('Actor2')
+# print(m.__repr__())
+m1 = Movie("Movie", 2019, 9.4)
+print(m.__gt__(m1))
