cca60c2a981346ca113ad7da26cec79dacc95f7b
VelkovIv <ivaylo.velkov@gmail.com>
2023-04-06 23:08:08 +0300

    Create astronaut_repository.py

---
+++ Python-OOP/Exam-Preparation/23.08.2021/01. Structure_Skeleton/project/astronaut/astronaut_repository.py
@@ -0,0 +1,18 @@
+from typing import List
+from project.astronaut.astronaut import Astronaut
+
+
+class AstronautRepository:
+    def __init__(self):
+        self.astronauts: List[Astronaut] = []
+
+    def add(self, astronaut: Astronaut):
+        self.astronauts.append(astronaut)
+
+    def remove(self, astronaut: Astronaut):
+        self.astronauts.remove(astronaut)
+
+    def find_by_name(self, name: str):
+        for astronaut in self.astronauts:
+            if astronaut.name == name:
+                return astronaut
